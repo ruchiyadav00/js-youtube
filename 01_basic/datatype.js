d503f068b3472250f5df181bed3464f97473b92f
ruchiyadav00 <145835786+ruchiyadav00@users.noreply.github.com>
2023-10-08 19:59:50 +0000

    operation, conparision, and conversion

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -6,9 +6,33 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = "";
 let BooleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
-1 => true; 0 => false;
-"" => false; "hitesh" => true;
+// 1 => true; 0 => false;
+// "" => false; "hitesh" => true;
+
+// **************** operations **********************************
+
+let num = 33;
+let negNum = -num;
+console.log(negNum);
+
+// addition (2+2)
+// subtract (3-2)
+// multiply (3*2)
+// divide (6/2)
+// power (2**5)
+// reminder(2%3)
+
+let str1 = "hello";
+let str2 = " raj";
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log( null > 0);
+console.log(null == 0);
+console.log( null >= 0);
+
+
 
 
