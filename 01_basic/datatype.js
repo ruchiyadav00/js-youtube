e95badfabba6556972ad41d03d44be62c182b2bd
ruchiyadav00 <145835786+ruchiyadav00@users.noreply.github.com>
2023-10-08 18:27:43 +0000

    convertion of datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,14 @@
+let score = "abs";
+// console.log( typeof score);
+let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "";
+let BooleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(BooleanIsLoggedIn);
+
+1 => true; 0 => false;
+"" => false; "hitesh" => true;
+
+
